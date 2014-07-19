fbf037b9331994849d5b26dde01708b2cb2c216a
Rodolfo Castellanos <rodolfojcj@yahoo.com>
2014-07-19 19:23:39 +0000

    Updated version in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rodolfojcj@yahoo.com'
 license          'Apache v2.0'
 description      'Installs and configures SOGo groupware server solution'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.1'
+version          '0.3.2'
 
 depends "apt"
 depends "apache2"
